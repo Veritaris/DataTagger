8b7cda0579cafbac916ff47ea9556c1a7d981598
georgii.imeshkenov <georgii.imeshkenov@mati.io>
2022-02-14 18:08:40 +0300

    made some abstract to move close for a new annotation type (points on image, sound and etc)

--- src/main/java/me/veritaris/datatagger/Controllers/ImageTaggingController.java
+++ src/main/java/me/veritaris/datatagger/Controllers/ImageTaggingController.java
@@ -28,25 +28,81 @@ public class ImageTaggingController extends Controller implements Initializable 
 
     @FXML protected Button repeatedImageButton;
     @FXML protected Button finishTaggingButton;
-    @FXML protected TextField captchaTextField;
+    @FXML protected TextField annotationTextField;
     @FXML protected ProgressBar datasetTaggingProgress;
     @FXML protected Button goToNextImageButton;
-    @FXML protected ImageView captchaImage;
+    @FXML protected ImageView imageToAnnotate;
     @FXML protected AnchorPane textTaggingPane;
     @FXML protected Label currentFilePath;
+    @FXML protected ListView<Annotation> imagesList;
 
     private final KeyCodeCombination openMetadataFileCombination = new KeyCodeCombination(KeyCode.O, KeyCombination.ALT_DOWN);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         int lastTaggedImage = Model.getMetadata().getLastTaggedImage();
+
         datasetTaggingProgress.setProgress(Model.getMetadata().getLastTaggedImage() / (double)Model.getMetadata().getImagesAmount());
         datasetTaggingProgress.setTooltip(new Tooltip(String.format("%s/%s", Model.getMetadata().getLastTaggedImage(), Model.getMetadata().getImagesAmount())));
 
+        updateAnnotationList();
+        imagesList.scrollTo(Model.getMetadata().getLastTaggedImage() - 4);
+
         if (Model.getMetadata().getTaggedImages().containsKey(lastTaggedImage)) {
-            captchaTextField.setText(Model.getMetadata().getTaggedImages().get(lastTaggedImage));
+            annotationTextField.setText(Model.getMetadata().getTaggedImages().get(lastTaggedImage));
         }
 
+        setupListViewEvents();
+        setupTextFieldEvents();
+        setupCommonEvents();
+
+        openImage();
+    }
+
+
+    private void setupTextFieldEvents() {
+        annotationTextField.setOnKeyPressed(
+            (event) -> {
+                switch (event.getCode()) {
+                    case ENTER:
+                        handleTagAndGoToNextImage();
+                        break;
+                    case TAB:
+                        switchFocus();
+                        break;
+                }
+            }
+        );
+    }
+
+    private void setupListViewEvents() {
+        imagesList.setOnMouseClicked(
+            (event) -> {
+                if (event.getButton().equals(MouseButton.PRIMARY)) {
+                    if (event.getClickCount() == 2) {
+                        Annotation selectedAnnotation = imagesList.getSelectionModel().getSelectedItems().get(0);
+                        openImage(selectedAnnotation.getId());
+                        annotationTextField.setText(selectedAnnotation.getAnnotation());
+                    }
+                }
+                event.consume();
+            }
+        );
+        imagesList.setOnKeyPressed(
+            (event) -> {
+                switch (event.getCode()) {
+                    case ENTER:
+                        Annotation selectedAnnotation = imagesList.getSelectionModel().getSelectedItems().get(0);
+                        openImage(selectedAnnotation.getId());
+                        annotationTextField.setText(selectedAnnotation.getAnnotation());
+                        break;
+                }
+            }
+        );
+    }
+
+    private void setupCommonEvents() {
         textTaggingPane.setOnKeyPressed(
             (event) -> {
                 if (openMetadataFileCombination.match(event)) {
@@ -56,14 +112,14 @@ public class ImageTaggingController extends Controller implements Initializable 
                 switch (event.getCode()) {
                     case LEFT:
                     case KP_LEFT:
-                        if (captchaTextField.isFocused()) {
+                        if (annotationTextField.isFocused()) {
                             return;
                         }
                         handleChangeImage(-1);
                         break;
                     case RIGHT:
                     case KP_RIGHT:
-                        if (captchaTextField.isFocused()) {
+                        if (annotationTextField.isFocused()) {
                             return;
                         }
                         handleChangeImage(+1);
@@ -105,13 +161,27 @@ public class ImageTaggingController extends Controller implements Initializable 
         }
     }
 
+    private void updateAnnotationList() {
+        imagesList.getItems().clear();
+        IntStream.rangeClosed(0, Model.getMetadata().getImagesAmount()-1).forEach(
+            (id) -> imagesList.getItems()
+                .add(new ImageTextAnnotation(id, Model.getMetadata().getTaggedImages().get(id)))
+        );
+    }
+
     private void switchFocus() {
-        if (captchaTextField.isFocused()) {
+        String textFieldData = annotationTextField.getText();
+        if (annotationTextField.isFocused()) {
             textTaggingPane.requestFocus();
         } else {
-            captchaTextField.requestFocus();
-            captchaTextField.deselect();
-            captchaTextField.positionCaret(captchaTextField.getText().length());
+            annotationTextField.requestFocus();
+            annotationTextField.deselect();
+
+            if (textFieldData != null) {
+                annotationTextField.positionCaret(textFieldData.length());
+            } else {
+                annotationTextField.positionCaret(0);
+            }
         }
     }
 
@@ -138,34 +208,67 @@ public class ImageTaggingController extends Controller implements Initializable 
             return;
         }
 
+        imagesList.getSelectionModel().select(imagesList.getSelectionModel().getSelectedIndex() + direction);
+        imagesList.scrollTo(imagesList.getSelectionModel().getSelectedIndex());
+
         Model.getMetadata().setLastTaggedImage(newImageId);
         Model.getInstance().saveProgress();
+
         openImage();
 
-        captchaTextField.setText(Model.getMetadata().getTaggedImages().getOrDefault(newImageId, ""));
+        annotationTextField.setText(Model.getMetadata().getTaggedImages().getOrDefault(newImageId, ""));
     }
 
     private void handleTagAndGoToNextImage() {
-        String tag = captchaTextField.getText();
-        if (!Model.getMetadata().getTaggedImages().containsValue(tag)) {
-            Model.writeImageTag(tag);
-            captchaTextField.clear();
-            openImage();
-            datasetTaggingProgress.setProgress(Model.getMetadata().getLastTaggedImage() / (double)Model.getMetadata().getImagesAmount());
-            datasetTaggingProgress.setTooltip(new Tooltip(String.format("%s/%s", Model.getMetadata().getLastTaggedImage(), Model.getMetadata().getImagesAmount())));
-            captchaTextField.requestFocus();
+        String annotation = annotationTextField.getText();
+
+        if (!Model.getMetadata().getTaggedImages().containsValue(annotation)) {
+            Model.writeImageTag(annotation);
+
+            saveAnnotation(annotation);
+            return;
         }
+
+        repeatedImageButton.setDisable(false);
+    }
+
+    private void saveAnnotation(String annotation) {
+        if (Model.getMetadata().isGitSavingEnabled()) {
+            Model.getGitHandler().handleChanges(new ImageTextAnnotation(Model.getMetadata().getLastTaggedImage(), annotation));
+        }
+
+        annotationTextField.clear();
+        openImage();
+        updateAnnotationList();
+        datasetTaggingProgress.setProgress(Model.getMetadata().getLastTaggedImage() / (double)Model.getMetadata().getImagesAmount());
+        datasetTaggingProgress.setTooltip(new Tooltip(String.format("%s/%s", Model.getMetadata().getLastTaggedImage(), Model.getMetadata().getImagesAmount())));
+        annotationTextField.requestFocus();
     }
 
     private void openImage() {
         try {
             File file = new File(Model.getLastTaggedImagePath());
-            this.captchaImage.setImage(new Image(new FileInputStream(Model.getLastTaggedImagePath())));
+
+            this.imageToAnnotate.setImage(new Image(new FileInputStream(Model.getLastTaggedImagePath())));
             this.currentFilePath.setText(
                 file.getParentFile().getName() + "/" + file.getName()
             );
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void openImage(int imageId) {
+        try {
+            File file = new File(Model.getDatasetPath() + "/" + imageId);
+            Model.getMetadata().setLastTaggedImage(imageId);
+
+            this.imageToAnnotate.setImage(new Image(new FileInputStream(Model.getLastTaggedImagePath())));
+            this.currentFilePath.setText(
+                file.getParentFile().getName() + "/" + file.getName()
+            );
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
         }
     }
 }
